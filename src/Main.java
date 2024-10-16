94c0bc8e80ee3eb28796669fb9db454ea50741b9
ECanDo <ecandoecantdo@gmail.com>
2024-10-15 22:56:29 -0600

    Added some code to test the functionality of the Matrix class.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Matrix matrix1, matrix2;
+        Matrix[] result;
+        matrix1 = new Matrix(
+                new double[][]{
+                        {1, 3, 2},
+                        {2, 1, 1},
+                        {4, 2, 3}
+                }
+        );
+        matrix2 = Matrix.identity(matrix1.rows());
+
+        result = matrix1.reducedRowEchelonForm(matrix2);
+        System.out.println("Reduced Row Echelon Form:\n" + result[0] + "==========\n" + result[1]);
+
+        System.out.println("Inverse:\n" + matrix1.inverse());
     }
+
+
 }
